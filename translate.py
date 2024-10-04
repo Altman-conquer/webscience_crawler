b85d8dc64e237fc150f8dca2e41015408d3a83ec
Altman <2476100824@qq.com>
2024-10-04 20:29:42 +0800

    Changes

--- translate.py
+++ translate.py
@@ -6,7 +6,7 @@ from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.tmt.v20180321 import tmt_client, models
 
-from settings import TENCENT_API_KEY, TENCENT_API_SECRET
+from settings import TENCENT_API_KEY, TENCENT_API_SECRET, AZURE_TRANSLATE_KEY
 
 
 def translate(text):
@@ -48,6 +48,43 @@ def translate(text):
         return ''
 
 
+def translate_azure(text):
+    import requests, uuid, json
+
+    # Add your key and endpoint
+    key = AZURE_TRANSLATE_KEY
+    endpoint = "https://api.cognitive.microsofttranslator.com"
+
+    # location, also known as region.
+    # required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+    location = "eastasia"
+
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    params = {
+        'api-version': '3.0',
+        'from': 'en',
+        'to': ['zh']
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        # location required if you're using a multi-service or regional (not global) resource.
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text': text
+    }]
+
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
